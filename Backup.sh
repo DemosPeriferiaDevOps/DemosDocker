c593faa31a84ffd44e4aad43a1dbfd0caf574f44
Eduar Ordoñez <eduarordonez@periferia-it.com>
2022-04-27 07:26:33 -0500

    backup

--- Backup.sh
+++ Backup.sh
@@ -1,12 +1,18 @@
-#Levantar Jenkins 
-docker run -d -u root -p 8080:8080 --name jenikns -v /var/jenkins:/var/jenkins_home jenkins/jenkins:latest
+# tar -czvf backup.tar.gz /var/jenkins/*
 
-tar -czvf backup.tar.gz /var/jenkins/*
-
-#subir backub
-scp -i DemosDocker/keybackup.pem backup/backup.tar.gz ubuntu@ec2-44-192-98-222.compute-1.amazonaws.com:/home/ubuntu/backupJenkins.tar
+# #subir backub
+# scp -i DemosDocker/keybackup.pem backup/backup.tar.gz ubuntu@ec2-44-192-98-222.compute-1.amazonaws.com:/home/ubuntu/backupJenkins.tar
 
 #dar permisos a clave
 chmod 400 DemosDocker/keybackup.pem
 #bajar backup
 scp -i DemosDocker/keybackup.pem ubuntu@ec2-44-192-98-222.compute-1.amazonaws.com:/home/ubuntu/backupJenkins.tar.gz .
+
+cp backupJenkins.tar.gz /var/jenkins/
+
+cd /var/jenikns/
+tar -xzvf backupJenkins.tar.gz
+rm backupJenkins.tar.gz
+cd
+#levantar Jenkins
+docker run -d -u root -p 8080:8080 --name jenkins -v /var/jenkins:/var/jenkins_home jenkins/jenkins:latest
